7c6aeb903e05511aadf7d3299e91306cbd8a564c
Joe <joefrizzell@gmail.com>
2020-11-22 13:11:50 -0500

    renaming admin to event admin, tweaking script to read from google sheets

--- event-admin/scripts.js
+++ event-admin/scripts.js
@@ -1,8 +1,7 @@
 /*
 Login authentication
 */
-var email = 'not signed in';
-var googleUser;
+var id_token;
 function onSignIn(googleUser) {
   window.googleUser = googleUser;
   var id_token = googleUser.getAuthResponse().id_token;
@@ -24,22 +23,35 @@ function onSignIn(googleUser) {
       document.getElementById('prompt').innerHTML = "Could not verify login, please try again. If the problem persists, please contact the developer.";
     }
   };
+  window.id_token = id_token;
   xhr.send('id_token=' + id_token);
 }
+/* Check email allowed */
 function checkAllowed(email_address){
   var xhr = new XMLHttpRequest();
   xhr.open('POST', 'https://gardenlifegame.com/megs_php/checkemail.php');
   xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
   xhr.onload = function() {
-    console.log('Response: ' + xhr.responseText);
     if(xhr.responseText.startsWith("0")){
       document.getElementById('access-response').innerHTML = xhr.responseText.substring(2);
+      loadSpreadSheetData();
     } else {
       document.getElementById('access-response').innerHTML = "Access Denied for " + email_address;
     }
   }
   xhr.send('email_address=' + email_address);
+  xhr.send('id_token=' + window.id_token);
 }
+function loadSpreadSheetData(){
+  var xhr = new XMLHttpRequest();
+  xhr.open('POST', 'https://gardenlifegame.com/megs_php/checkemail.php');
+  xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+  xhr.onload = function() {
+      document.getElementById('spreadsheet-data').innerHTML = xhr.responseText.substring(2);
+  };
+  xhr.send('id_token=' + window.id_token);
+}
+/* Sign out of Google*/
 function signOut() {
   window.googleUser = null;
   var auth2 = gapi.auth2.getAuthInstance();
@@ -49,6 +61,7 @@ function signOut() {
   document.getElementById('signed-in').style.display = "none";
   document.getElementById('signed-out').style.display = "grid";
   document.getElementById('access-response').innerHTML = "Access not checked";
+  window.id_token = "";
 }
 
 /*
