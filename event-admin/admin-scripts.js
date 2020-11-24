45d2b5e1ff8e7f9002790e666b45bbec32163da4
Joe <joefrizzell@gmail.com>
2020-11-24 00:32:46 -0500

    adding time conversion functions

--- event-admin/admin-scripts.js
+++ event-admin/admin-scripts.js
@@ -244,7 +244,26 @@ function calculateEndTime(){
   startTimePicker = document.getElementById("start-time");
   endTimePicker = document.getElementById("end-time");
   var row = defaultFieldValues[eventTypeValue]
-  defaultDuration = parseInt(row[1]) * 60;
-  endTimePicker.value = startTimePicker.value + defaultDuration;
+  defaultDuration = timeFromMins(parseInt(row[1]) * 60);
+  endTimePicker.value = addTimes(startTimePicker.value, defaultDuration);
   console.log(defaultDuration);
+}
+// Convert a time in hh:mm format to minutes
+function timeToMins(time) {
+  var b = time.split(':');
+  return b[0]*60 + +b[1];
+}
+
+// Convert minutes to a time in format hh:mm
+// Returned value is in range 00  to 24 hrs
+function timeFromMins(mins) {
+  function z(n){return (n<10? '0':'') + n;}
+  var h = (mins/60 |0) % 24;
+  var m = mins % 60;
+  return z(h) + ':' + z(m);
+}
+
+// Add two times in hh:mm format
+function addTimes(t0, t1) {
+  return timeFromMins(timeToMins(t0) + timeToMins(t1));
 }
